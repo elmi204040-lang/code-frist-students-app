435d2723ccdf6106c5e6576637a94a31eab6c0cd
elmi204040-lang <elmi204040@gmail.com>
2026-06-22 16:30:19 +0300

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstStudentApp
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
